c054d9b3ba293ad7db1db37dd879098138d60446
Himel Das (Spectre-X360) <himel.p.das@gmail.com>
2016-12-25 16:54:53 -0500

    https

--- applications/init/controllers/0.py
+++ applications/init/controllers/0.py
@@ -1,4 +1,4 @@
-response.view = 'templates/survey_extend.html'  # http://stackoverflow.com/questions/8750723/is-it-possible-to-change-a-web2py-view-on-the-fly
+response.view = os.path.join("templates", "survey_extend.html")  # http://stackoverflow.com/questions/8750723/is-it-possible-to-change-a-web2py-view-on-the-fly
 response.title = "PCMH Info"
 
 def index():
