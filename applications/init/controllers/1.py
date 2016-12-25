be67aa649f5d65316dc9c754a97b4bd22f37a52c
Himel Das (Spectre-X360) <himel.p.das@gmail.com>
2016-12-25 16:57:06 -0500

    os path error

--- applications/init/controllers/1.py
+++ applications/init/controllers/1.py
@@ -1,4 +1,4 @@
-response.view = 'templates/survey_extend.html'  # http://stackoverflow.com/questions/8750723/is-it-possible-to-change-a-web2py-view-on-the-fly
+response.view = os.path.join("templates", "survey_extend.html")  # http://stackoverflow.com/questions/8750723/is-it-possible-to-change-a-web2py-view-on-the-fly
 response.title = "PCMH 1"
 
 #@auth.requires(URL.verify(request, hash_vars=["app_id"], hmac_key=MY_KEY), requires_login=True)
