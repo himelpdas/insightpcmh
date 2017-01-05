8a428e49601223e2186fdcc66da6a7fe0919528b
Himel Das (Spectre-X360) <himel.p.das@gmail.com>
2017-01-05 10:03:15 -0500

    questions

--- applications/init/models/db_model_2.py
+++ applications/init/models/db_model_2.py
@@ -1,19 +1,23 @@
 ####Transition of Care Internal####
 db.define_table("transition_of_care_plan_internal",
+                Field("application", 'reference application', readable=False, writable=False),
                 _yes_no_field_default,
                 _note_field, auth.signature)
 
 db.define_table("intake_form",
+                Field("application", 'reference application', readable=False, writable=False),
                 _yes_no_field_default,
                 _note_field, auth.signature)
 
 db.define_table("intake_form_upload",
+                Field("application", 'reference application', readable=False, writable=False),
                 Field("upload", "upload", requires=IS_NOT_EMPTY(), label="Upload Intake Form"),
                 Field("filename", readable=False, writable=False),
                 _note_field,
                  auth.signature)
 
 db.define_table("intake_form_patient_example",
+                Field("application", 'reference application', readable=False, writable=False),
                 Field("patient_first_name", label="Patient First Name", requires=IS_NOT_EMPTY()),
                 Field("patient_last_name", label="Patient Last Name", requires=IS_NOT_EMPTY()),
                 Field("patient_DOB", "date", label="Patient DOB", requires=IS_NOT_EMPTY()),
