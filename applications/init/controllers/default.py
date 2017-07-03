28edcaf9ba692dfadf038305e1783af045f2e24e
Himel Das (Surface) <himel.p.das@gmail.com>
2017-07-02 22:43:03 -0400

    practice photo

--- applications/init/controllers/default.py
+++ applications/init/controllers/default.py
@@ -322,7 +322,7 @@ def certify():
     db.application.certified_on.readable = True
     db.application.practice_name.readable = True
     db.application.status.readable = True
-    db.application.status.readable = True
+    db.application.status.writable = True
 
     form = SQLFORM.grid(db.application, create=False, deletable=False)
 
