433c8d3a649562ae8c2b5136ebb589c9913f54f0
Himel Das (Surface) <himel.p.das@gmail.com>
2017-07-02 20:49:42 -0400

    logging

--- applications/init/models/db_logs.py
+++ applications/init/models/db_logs.py
@@ -1,9 +1,15 @@
 db.define_table("logging",
     Field("owner_id", db.auth_user, label="Author"),
-    Field("description", 'text'),
-    Field("difficulty", requires=IS_IN_SET(["High", "Medium", "Low"], zero=None)),
+    Field("description", 'text', requires=IS_NOT_EMPTY()),
+    Field("people_involved", 'list:string', requires=IS_NOT_EMPTY()),
+    Field("difficulty", requires=IS_IN_SET(["High", "Medium", "Low", "No Issue/Resolved"], zero=None)),
 )
 
 db.logging.application.widget = SQLFORM.widgets.autocomplete(
     request, db.application.practice_name, limitby=(0, 10), min_length=0, distinct=True,
     id_field=db.application.id)
+
+db.logging.application.readable = True
+db.logging.application.writable = True
+
+db.logging._enable_record_versioning()
