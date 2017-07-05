2760cc077e77ed7b22dadc2c20f8d26e5869ee0e
Himel Das (Surface) <himel.p.das@gmail.com>
2017-07-04 20:36:52 -0400

    doc

--- applications/init/controllers/2014.py
+++ applications/init/controllers/2014.py
@@ -425,6 +425,7 @@ def pcmh_1_1a__5():
         dict(
             description="1A5 Monitoring No-Show Policy.doc",
             url=URL('init', 'word', 'no_show_policy.doc',
+                    args=["1A5 Monitoring No-Show Policy.doc"],
                     vars=dict(**request.get_vars), hmac_key=MY_KEY, salt=session.MY_SALT, hash_vars=["app_id"]),
             permissions=["IS_MANAGER"]
         ),
