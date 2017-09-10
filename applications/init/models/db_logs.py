24807106c0badb582a18dea0f1f52efe9c837d1c
Himel Das (Surface) <himel.p.das@gmail.com>
2017-09-10 14:09:17 -0400

    sort

--- applications/init/models/db_logs.py
+++ applications/init/models/db_logs.py
@@ -2,14 +2,14 @@ db.define_table(
     "logging",
     Field("owner_id", db.auth_user, label="Author"),
     Field("people_involved", 'list:string', requires=IS_NOT_EMPTY()),
-    Field("contact_date", "date", label="Service Date", requires=DATE_VALIDATOR),
+    Field("contact_date", "date", label="Contact Date", requires=DATE_VALIDATOR),
     Field("start_time", "time", requires=AM_PM_VALIDATOR),
     Field("end_time", "time", requires=AM_PM_VALIDATOR),
     Field("contact_type", requires=IS_IN_SET(["Meeting", "Telephone", "Email", "SMS"], zero=None)),
     Field("problem_or_canceled", "boolean"),
     Field("goals_met", 'text', requires=IS_NOT_EMPTY()),
     Field("goals_not_met", 'text', requires=IS_NOT_EMPTY()),
-    Field("follow_up_date", "date", label="Service Date", requires=DATE_VALIDATOR),
+    Field("follow_up_date", "date", label="Follow-up Date", requires=DATE_VALIDATOR),
     Field("follow_up_time", "time", requires=AM_PM_VALIDATOR),
 )
 
