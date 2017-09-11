2de8ffb79636eeb61885b7f068ee0a146aae1215
Himel Das (Surface) <himel.p.das@gmail.com>
2017-09-11 01:35:52 -0400

    upload fix

--- applications/init/models/menu.py
+++ applications/init/models/menu.py
@@ -53,6 +53,8 @@ IS_TRAINER = auth.has_membership("trainers")
 IS_CONTRIB = auth.has_membership("contributors")
 IS_STAFF = IS_TRAINER or IS_MANAGER or IS_ADMIN
 IS_TEAM = IS_STAFF or IS_CONTRIB
+NOT_GRID = "edit" in request.args or "view" in request.args
+IS_GRID = not NOT_GRID
 
 if IS_MASTER or IS_ADMIN:
     db.auth_user.is_insight.readable = True
